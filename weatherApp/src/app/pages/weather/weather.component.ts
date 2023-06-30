c6dfe5e90dff3735c75d41679b2e2e48fe296b56
Chetana-Moreshwar-Bhanarkar <chetana27bhanarkar@gmail.com>
2023-06-30 11:10:58 +0530

    weather ts

---
+++ weatherApp/src/app/pages/weather/weather.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { WeatherService } from 'src/app/service/weather.service';
+
+@Component({
+  selector: 'app-weather',
+  templateUrl: './weather.component.html',
+  styleUrls: ['./weather.component.css']
+})
+export class WeatherComponent{
+  city!:string
+  weatherData : any ;
+
+  constructor(private weatherService : WeatherService){}
+
+  ngOnInit(): void {}
+
+  getWeather(){
+    this.weatherService.getWeather(this.city).subscribe(data => {
+      this.weatherData = data ;
+      console.log(data);
+
+    })
+  }
+}
