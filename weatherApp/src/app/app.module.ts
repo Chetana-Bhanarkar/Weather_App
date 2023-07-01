403b5852074fdb1d79351e25cadda7c50c828b1a
Chetana-Moreshwar-Bhanarkar <chetana27bhanarkar@gmail.com>
2023-07-01 17:31:13 +0530

    app module

---
+++ weatherApp/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { WeatherComponent } from './pages/weather/weather.component';
+import { WeatherService } from './service/weather.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    WeatherComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [WeatherService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
